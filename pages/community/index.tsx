279efa49991063f3c4de6fd9a780eb22d1fb91c8
mrasilbek19 <asilbek190303@gmail.com>
2026-09-12 19:15:40 +0900

    feat: integration header and footer via layouts

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,20 +1,17 @@
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { NextPage } from "next";
 import { useState } from "react";
 
-const Community = () => {
-    console.log("COMMUNITY COMPONENT - PAGES ROUTER");
+const Community: NextPage = () => {
     const [title, setTitle] = useState<string>("hello");
-
     return (
         <div>
             COMMUNITY{" "}
-            <button
-                onClick={() => alert("Hello MIT")}
-                style={{ margin: "15px" }}
-            >
+            <button onClick={() => alert("Hello MIT")} style={{ margin: "15px" }}>
                 PressMe
             </button>
         </div>
     );
 };
 
-export default Community;
+export default withLayoutBasic(Community);
--- pages/property/detail.tsx
+++ pages/property/detail.tsx
@@ -2,7 +2,7 @@ import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
 import { Container } from "@mui/material";
 import { NextPage } from "next";
 
-const PropertyList: NextPage = () => {
-    return <Container>Property List</Container>;
+const PropertyDetail: NextPage = () => {
+    return <Container>Property detail</Container>;
 };
-export default withLayoutBasic(PropertyList);
+export default withLayoutBasic(PropertyDetail);
