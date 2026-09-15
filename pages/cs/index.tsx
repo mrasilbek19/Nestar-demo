58b61b67337c12b41a49e9f9aeba673fb949ab15
mrasilbek19 <asilbek190303@gmail.com>
2026-09-15 15:38:39 +0900

    feat: integration of publishing sources

--- pages/cs/index.tsx
+++ pages/cs/index.tsx
@@ -2,19 +2,20 @@ import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
 import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
 import { Stack } from "@mui/material";
 import { NextPage } from "next";
+import { useState } from "react";
 
-const Community: NextPage = () => {
+const CS: NextPage = () => {
   const device = useDeviceDetect();
 
   if (device === "mobile") {
-    return <Stack>COMMUNITY MOBILE</Stack>;
+    return <Stack>CS MOBILE</Stack>;
   } else {
     return (
       <div style={{ margin: "20px 0" }}>
-        <Stack className="container">COMMUNITY</Stack>
+        <Stack className="container">CS</Stack>
       </div>
     );
   }
 };
 
-export default withLayoutBasic(Community);
+export default withLayoutBasic(CS);
